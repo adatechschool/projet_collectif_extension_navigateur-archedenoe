3f6dceda404d65505529e472dd920a898ef35a26
HelderF2821 <helder.ferreira2821@gmail.com>
2023-01-17 15:41:37 +0100

    tkt

--- script.js
+++ script.js
@@ -130,21 +130,61 @@ for (let i = 0; i < allButtons.length; i++) {
 
 //Création des divs infos pour l'API2 (news)
 let divnews = document.createElement("div");
-divnews.className = "Info";
+divnews.className = "Info left";
 document.body.appendChild(divnews);
 
 let divnews2 = document.createElement("div");
-divnews2.className = "Info";
+divnews2.className = "Info left";
 document.body.appendChild(divnews2);
 
 let divnews3 = document.createElement("div");
-divnews3.className = "Info";
-
-let allDivs = document.getElementsByClassName("Info");
-console.log({ allDivs });
+divnews3.className = "Info left";
 document.body.appendChild(divnews3);
 
-// CSS DivNews
+//Création des divs infos pour vautour (news)
+let divnews4 = document.createElement("div");
+divnews4.className = "Info left";
+document.body.appendChild(divnews4);
+
+let divnews5 = document.createElement("div");
+divnews5.className = "Info left";
+document.body.appendChild(divnews5);
+
+let divnews6 = document.createElement("div");
+divnews6.className = "Info left";
+document.body.appendChild(divnews6);
+
+//Création des divs infos pour coccinelle (news)
+let divnews7 = document.createElement("div");
+divnews7.className = "Info right";
+document.body.appendChild(divnews7);
+
+let divnews8 = document.createElement("div");
+divnews8.className = "Info right";
+document.body.appendChild(divnews8);
+
+let divnews9 = document.createElement("div");
+divnews9.className = "Info right";
+document.body.appendChild(divnews9);
+
+//Création des divs infos pour lynx (news)
+let divnews10 = document.createElement("div");
+divnews10.className = "Info right";
+document.body.appendChild(divnews10);
+
+let divnews11 = document.createElement("div");
+divnews11.className = "Info right";
+document.body.appendChild(divnews11);
+
+let divnews12 = document.createElement("div");
+divnews12.className = "Info right";
+document.body.appendChild(divnews12);
+
+let allDivs = document.getElementsByClassName("Info left");
+let allDivs2 = document.getElementsByClassName("Info right");
+
+
+// CSS allDivs
 for (let i = 0; i < allDivs.length; i++) {
   allDivs[i].style.display = "inline";
   allDivs[i].style.backgroundColor = "#fce3ad";
@@ -154,15 +194,42 @@ for (let i = 0; i < allDivs.length; i++) {
   allDivs[i].style.borderRadius = "10px";
   allDivs[i].style.position = "fixed";
   allDivs[i].style.zIndex = "100";
-  allDivs[i].style.left = "-100px";
+  allDivs[i].style.left = "-330px";
+  allDivs[i].style.maxWidth = "300px";
+}
+
+// CSS allDivs2
+for (let i = 0; i < allDivs2.length; i++) {
+  allDivs2[i].style.display = "inline";
+  allDivs2[i].style.backgroundColor = "#fce3ad";
+  allDivs2[i].style.padding = "5px";
+  allDivs2[i].style.fontSize = "18px";
+  allDivs2[i].style.border = "#f3ad6b 2px solid";
+  allDivs2[i].style.borderRadius = "10px";
+  allDivs2[i].style.position = "fixed";
+  allDivs2[i].style.zIndex = "100";
+  allDivs2[i].style.right = "-330px";
+  allDivs2[i].style.maxWidth = "300px";
 }
 
 divnews.style.bottom = "100px";
 divnews2.style.bottom = "200px";
 divnews3.style.bottom = "300px";
 
-// Fonction API2 (news) pour fetch les infos et remplir les divs
-function Newanimal() {
+divnews4.style.bottom = "400px";
+divnews5.style.bottom = "500px";
+divnews6.style.bottom = "600px";
+
+divnews7.style.bottom = "100px";
+divnews8.style.bottom = "200px";
+divnews9.style.bottom = "300px";
+
+divnews10.style.bottom = "400px";
+divnews11.style.bottom = "500px";
+divnews12.style.bottom = "600px";
+
+// Fonction API2 (news) pour fetch les infos et remplir les divs seahorse
+function seahorseFact() {
   let nom = "seahorse";
 
   fetch("https://api.api-ninjas.com/v1/animals?name=" + nom, {
@@ -176,25 +243,105 @@ function Newanimal() {
     .then(function (value) {
       // Mettre les infos dans les divs
       divnews.innerHTML =
-        "Régime alimentaire : " + value[0].characteristics.diet;
+        "Seahorse = Distinctive feature : " + value[0].characteristics.distinctive_feature;
 
       divnews2.innerHTML =
-        "Régime alimentaire 2 :" + value[0].characteristics.diet;
+        "Seahorse = Favorite food : " + value[0].characteristics.favorite_food;
 
       divnews3.innerHTML =
-        "Régime alimentaire 3 :" + value[0].characteristics.diet;
+        "Seahorse = Fun fact : " + value[0].characteristics.slogan;
     });
 }
-Newanimal();
+seahorseFact();
 
+// Fonction API2 (news) pour fetch les infos et remplir les divs vautour
+function vultureFact() {
+  let nom = "vulture";
+
+  fetch("https://api.api-ninjas.com/v1/animals?name=" + nom, {
+    headers: { "X-Api-Key": "A8Se3TXGr1J+WsnbVJEwSw==GikJKEs7eqMvdvLx" },
+  })
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (value) {
+      // Mettre les infos dans les divs
+      divnews4.innerHTML =
+        "Vulture = Main prey : " + value[6].characteristics.main_prey;
+
+      divnews5.innerHTML =
+        "Vulture = Lifestyle : " + value[6].characteristics.lifestyle;
+
+      divnews6.innerHTML =
+        "Vulture = Fun fact : " + value[6].characteristics.slogan;
+    });
+}
+vultureFact();
+
+
+// Fonction API2 (news) pour fetch les infos et remplir les divs coccinelle
+function ladybugFact() {
+  let nom = "ladybug";
+
+  fetch("https://api.api-ninjas.com/v1/animals?name=" + nom, {
+    headers: { "X-Api-Key": "A8Se3TXGr1J+WsnbVJEwSw==GikJKEs7eqMvdvLx" },
+  })
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (value) {
+      // Mettre les infos dans les divs
+      divnews7.innerHTML =
+        "Ladybug = Biggest threat : " + value[0].characteristics.biggest_threat;
+
+      divnews8.innerHTML =
+        "Ladybug = Age of sexual maturity : " + value[0].characteristics.age_of_sexual_maturity;
+
+      divnews9.innerHTML =
+        "Ladybug = Number of species : " + value[0].characteristics.number_of_species;
+    });
+}
+ladybugFact();
+
+// Fonction API2 (news) pour fetch les infos et remplir les divs lynx
+function lynxFact() {
+  let nom = "lynx";
+
+  fetch("https://api.api-ninjas.com/v1/animals?name=" + nom, {
+    headers: { "X-Api-Key": "A8Se3TXGr1J+WsnbVJEwSw==GikJKEs7eqMvdvLx" },
+  })
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then(function (value) {
+      // Mettre les infos dans les divs
+      divnews10.innerHTML =
+        "Lynx = Habitat : " + value[3].characteristics.habitat;
+
+      divnews11.innerHTML =
+        "Lynx = Lifespan : " + value[3].characteristics.lifespan;
+
+      divnews12.innerHTML =
+        "Lynx = Fun fact : " + value[3].characteristics.slogan;
+    });
+}
+lynxFact();
+
+//déplacement div gauches
 let statut = 0;
 
 function arrivingFromLeft(allDivs) {
   for (let k = 0; k < allDivs.length; k++) {
-    var pos = -100;
+    var pos = -330;
     var id = setInterval(frame, 10);
     function frame() {
-      if (pos == 350) {
+      if (pos == 310) {
         clearInterval(id);
       } else {
         pos++;
@@ -207,10 +354,10 @@ function arrivingFromLeft(allDivs) {
 
 function leavingToLeft(allDivs) {
   for (let k = 0; k < allDivs.length; k++) {
-    var pos = 340;
+    var pos = 310;
     var id = setInterval(frame, 10);
     function frame() {
-      if (pos == -100) {
+      if (pos == -330) {
         clearInterval(id);
       } else {
         pos--;
@@ -221,8 +368,41 @@ function leavingToLeft(allDivs) {
   statut = 0;
 }
 
+//déplacement div droites
+function arrivingFromRight(allDivs2) {
+  for (let k = 0; k < allDivs2.length; k++) {
+    var pos = -330;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (pos == 310) {
+        clearInterval(id);
+      } else {
+        pos++;
+        allDivs2[k].style.right = pos + "px";
+      }
+    }
+  }
+  statut = 1;
+}
+
+function leavingToRight(allDivs2) {
+  for (let k = 0; k < allDivs2.length; k++) {
+    var pos = 310;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (pos == -330) {
+        clearInterval(id);
+      } else {
+        pos--;
+        allDivs2[k].style.right = pos + "px";
+      }
+    }
+  }
+  statut = 0;
+}
+
 // // Création de la fonction AFK puis appel
-let inactivityTime = function (allDivs) {
+let inactivityTime = function (allDivs, allDivs2) {
   window.onload = deplacements;
   // document.onmousemove = deplacements;
   document.onkeypress = deplacements;
@@ -231,16 +411,16 @@ let inactivityTime = function (allDivs) {
     if (statut == 0) {
       setTimeout(function () {
         arrivingFromLeft(allDivs);
-        console.log(statut);
+        arrivingFromRight(allDivs2);
       }, 2000);
     } else {
-      console.log("ELSE");
       leavingToLeft(allDivs);
+      leavingToRight(allDivs2);
     }
   }
   setTimeout(() => {
-    inactivityTime(allDivs);
+    inactivityTime(allDivs, allDivs2);
   }, 1000);
 };
 
-inactivityTime(allDivs);
+inactivityTime(allDivs, allDivs2);
