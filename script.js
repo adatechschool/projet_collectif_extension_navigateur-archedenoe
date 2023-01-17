b9880e2c8f1c541cd139484a9a0ecf74cbfc4ebd
Dre-Drey <51761267+Dre-Drey@users.noreply.github.com>
2023-01-17 10:48:31 +0100

    en cours

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Code Audrey-Timer
-
+// Recuperer l'id
 let ExtensionId = chrome.runtime.id;
 
 //Fonction pour avoir un nombre random
@@ -33,7 +32,20 @@ function changeimage(animal) {
       }
     });
 }
+// Creation d'une div pour les boutons
+var divButtons = document.createElement("div");
+divButtons.className = "div-buttons";
+divButtons.style.backgroundColor = "black";
+divButtons.style.position = "fixed";
+divButtons.style.zIndex = "100";
+divButtons.style.bottom = "0px";
+divButtons.style.left = "50%";
+divButtons.style.transform = "translateX(-50%)";
+divButtons.style.border = "3px solid white";
+divButtons.style.borderRadius = "20px";
+document.body.appendChild(divButtons);
 
+// changer la couleur des boutons
 function changeButton(btn) {
   if (btn.style.backgroundColor == "black") {
     for (let i = 0; i < allButtons.length; i++) {
@@ -47,19 +59,6 @@ function changeButton(btn) {
     btn.style.backgroundColor = "black";
   }
 }
-
-// Creation d'une div
-var divButtons = document.createElement("div");
-divButtons.className = "div-buttons";
-divButtons.style.backgroundColor = "black";
-divButtons.style.position = "fixed";
-divButtons.style.zIndex = "100";
-divButtons.style.bottom = "0px";
-divButtons.style.left = "50%";
-divButtons.style.transform = "translateX(-50%)";
-divButtons.style.border = "3px solid white";
-divButtons.style.borderRadius = "20px";
-document.body.appendChild(divButtons);
 
 // Creation des boutons
 let button1 = document.createElement("button");
@@ -126,3 +125,127 @@ for (let i = 0; i < allButtons.length; i++) {
   allButtons[i].style.backgroundColor = "black";
   allButtons[i].style.border = "2px solid white";
 }
+
+//Création des divs infos pour l'API2 (news)
+let divnews = document.createElement("div");
+divnews.className = "Info";
+
+let divnews2 = document.createElement("div");
+divnews2.className = "Info";
+
+let divnews3 = document.createElement("div");
+divnews3.className = "Info";
+
+let allDivs = document.getElementsByClassName("Info");
+console.log({ allDivs });
+console.log(allDivs.length);
+
+// CSS DivNews
+for (let i = 0; i < allDivs.length; i++) {
+  allDivs[i].style.display = "inline";
+  allDivs[i].style.backgroundColor = "#fce3ad";
+  allDivs[i].style.padding = "5px";
+  allDivs[i].style.fontSize = "18px";
+  allDivs[i].style.border = "#f3ad6b 2px solid";
+  allDivs[i].style.borderRadius = "10px";
+  allDivs[i].style.position = "fixed";
+  allDivs[i].style.zIndex = "100";
+  allDivs[i].style.left = "-100px";
+}
+
+divnews.style.bottom = "100px";
+divnews2.style.bottom = "200px";
+divnews3.style.bottom = "300px";
+
+// Fonction API2 (news) pour fetch les infos et remplir les divs
+// function Newanimal() {
+//   let nom = "seahorse";
+
+//   fetch("https://api.api-ninjas.com/v1/animals?name=" + nom, {
+//     headers: { "X-Api-Key": "A8Se3TXGr1J+WsnbVJEwSw==GikJKEs7eqMvdvLx" },
+//   })
+//     .then(function (res) {
+//       if (res.ok) {
+//         return res.json();
+//       }
+//     })
+//     .then(function (value) {
+//       // Mettre les infos dans les divs
+//       divnews.innerHTML =
+//         "Régime alimentaire : " + value[0].characteristics.diet;
+//       document.body.appendChild(divnews);
+
+//       divnews2.innerHTML =
+//         "Régime alimentaire 2 :" + value[0].characteristics.diet;
+//       document.body.appendChild(divnews2);
+
+//       divnews3.innerHTML =
+//         "Régime alimentaire 3 :" + value[0].characteristics.diet;
+//       document.body.appendChild(divnews3);
+//     });
+// }
+// Newanimal();
+
+// // Création de la fonction AFK puis appel
+// let inactivityTime = function (allDivs) {
+//   // let time;
+//   function deplacements() {
+//     // clearTimeout(time);
+//     let time = setTimeout(function () {
+//       // clearTimeout(time);
+//       if (statut == 0) {
+//         arrivingFromLeft(allDivs);
+//       } else if (statut == 1) {
+//         console.log("test");
+//         document.onmousemove = leavingToLeft(allDivs);
+//         document.onkeypress = leavingToLeft(allDivs);
+//       } else {
+//         alert("Vous ête AFK");
+//       }
+//     }, 1000);
+//   }
+//   window.onload = deplacements;
+//   document.onmousemove = deplacements;
+//   document.onkeypress = deplacements;
+// };
+
+// inactivityTime(allDivs);
+
+// let statut = 0;
+
+// function arrivingFromLeft(allDivs) {
+//   return function () {
+//     for (let k = 0; k < allDivs.length; k++) {
+//       var pos = -100;
+//       var id = setInterval(frame, 10);
+//       function frame() {
+//         if (pos == 350) {
+//           console.log("test");
+//           clearInterval(id);
+//         } else {
+//           pos++;
+//           allDivs[k].style.left = pos + "px";
+//         }
+//       }
+//     }
+//     statut = 1;
+//   };
+// }
+
+// function leavingToLeft(allDivs) {
+//   return function () {
+//     for (let k = 0; k < allDivs.length; k++) {
+//       var pos = 340;
+//       var id = setInterval(frame, 10);
+//       function frame() {
+//         if (pos == -100) {
+//           clearInterval(id);
+//         } else {
+//           pos--;
+//           allDivs[k].style.left = pos + "px";
+//         }
+//       }
+//     }
+//     statut = 2;
+//   };
+// }
